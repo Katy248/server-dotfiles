251229389a3c518063a0e17cdc420a3354d0d71c
Katy248 <petrovanton247@gmail.com>
2024-04-27 13:15:48 +0300

    Update packages list

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
-dnf install i3 alacritty dunst rofi polybar neovim jetbrains-mono-fonts feh i3blocks stow
+dnf install i3 alacritty dunst rofi neovim jetbrains-mono-fonts feh i3blocks stow python pip
 
 pip install --user i3-layouts
